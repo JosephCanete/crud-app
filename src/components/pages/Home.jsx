3e69f03d25345c5f3c96c56daff60981d1998f19
Raymond Joseph Cañete <raymondcanete1997@gmail.com>
2022-07-20 13:05:11 +0800

    Added formik and yup

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Navigation from "./Navigation";
 import { useSelector } from "react-redux";
+import Input from "../forms/Input";
 
 export default function Home() {
   const counterFromHome = useSelector((state) => state.counter);
@@ -8,6 +9,7 @@ export default function Home() {
       <h1>Home Component</h1>
       <h4>Let's render counter as well {counterFromHome}</h4>
       <Navigation />
+      <Input label="Name" />
     </>
   );
 }
