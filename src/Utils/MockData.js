10e5bafbb0ba0a281fda8dc78960f43e8c89e9d5
Raymond Joseph Cañete <raymondcanete1997@gmail.com>
2022-07-30 11:39:57 +0800

    Added textarea and mockdata

---
+++ src/Utils/MockData.js
@@ -0,0 +1,70 @@
+export const mockContent = [
+  {
+    headerTitle: "What is your name?",
+    questionType: "single",
+    answer: "",
+    required: "true",
+  },
+  {
+    headerTitle: "What do you want to be when you grow up?",
+    questionType: "multi",
+    answer: [
+      "Web Developer",
+      "Node Js Developer",
+      "Data Analyst",
+      "React Developer",
+    ],
+    required: "true",
+  },
+  {
+    headerTitle: "What is your name?",
+    questionType: "single",
+    answer: "",
+    required: "true",
+  },
+  {
+    headerTitle: "What do you want to be when you grow up?",
+    questionType: "multi",
+    answer: [
+      "Web Developer",
+      "Node Js Developer",
+      "Data Analyst",
+      "React Developer",
+    ],
+    required: "true",
+  },
+  {
+    headerTitle: "What is your name?",
+    questionType: "single",
+    answer: "",
+    required: "true",
+  },
+  {
+    headerTitle: "What do you want to be when you grow up?",
+    questionType: "multi",
+    answer: [
+      "Web Developer",
+      "Node Js Developer",
+      "Data Analyst",
+      "React Developer",
+    ],
+    required: "true",
+  },
+  {
+    headerTitle: "What is your name?",
+    questionType: "single",
+    answer: "",
+    required: "true",
+  },
+  {
+    headerTitle: "What do you want to be when you grow up?",
+    questionType: "multi",
+    answer: [
+      "Web Developer",
+      "Node Js Developer",
+      "Data Analyst",
+      "React Developer",
+    ],
+    required: "true",
+  },
+];
