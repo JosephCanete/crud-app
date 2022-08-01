681efada7ed4e7e92a8a5213191be648bc565c05
Raymond Joseph Cañete <raymondcanete1997@gmail.com>
2022-08-01 23:23:49 +0800

    Update the code on cards

--- src/components/cards/CardChild.jsx
+++ src/components/cards/CardChild.jsx
@@ -4,38 +4,41 @@ import RadioGroup from "@mui/material/RadioGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Radio from "@mui/material/Radio";
 import TextField from "@mui/material/TextField";
+import CardContent from "@mui/material/CardContent";
+import FormControl from "@mui/material/FormControl";
 
 export default function CardChild({ item }) {
   return (
-    <>
-      {item.questionType === "multi" ? (
-        <>
-          <FormLabel>Select one answer below:</FormLabel>
-          {item.answer.map((answer, index) => (
-            <React.Fragment key={index}>
-              <RadioGroup defaultValue="null" name="radio-buttons-group">
-                <FormControlLabel
-                  key={index}
-                  value={answer}
-                  control={<Radio />}
-                  label={answer}
-                />
-              </RadioGroup>
-            </React.Fragment>
-          ))}
-        </>
-      ) : (
-        <>
-          <FormLabel>Answer here:</FormLabel>
-          <TextField
-            multiline={true}
-            id="standard-basic"
-            variant="standard"
-            size="medium"
-            sx={{ width: "50rem" }}
-          />
-        </>
-      )}
-    </>
+    <CardContent>
+      <FormControl>
+        {item.questionType === "multi" ? (
+          <>
+            <FormLabel>Select one answer below:</FormLabel>
+            {item.answer.map((answer, index) => (
+              <React.Fragment key={index}>
+                <RadioGroup defaultValue="null" name="radio-buttons-group">
+                  <FormControlLabel
+                    value={answer}
+                    control={<Radio />}
+                    label={answer}
+                  />
+                </RadioGroup>
+              </React.Fragment>
+            ))}
+          </>
+        ) : (
+          <>
+            <FormLabel>Answer here:</FormLabel>
+            <TextField
+              multiline={true}
+              id="standard-basic"
+              variant="standard"
+              size="medium"
+              sx={{ width: "50rem" }}
+            />
+          </>
+        )}
+      </FormControl>
+    </CardContent>
   );
 }
