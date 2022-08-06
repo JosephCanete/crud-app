b5abbabc4ecc02dab6c615db4d8c3b6f27db9128
Raymond Joseph Cañete <raymondcanete1997@gmail.com>
2022-08-06 14:00:38 +0800

    Started create form components

--- src/components/pages/CreateForm.jsx
+++ src/components/pages/CreateForm.jsx
@@ -1,3 +1,55 @@
+import { useState } from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+
 export default function CreateForm() {
-  return <div>CreateForm</div>;
+  const [cardList, setCardList] = useState([]);
+
+  const spawnCards = () => {
+    return setCardList((prevValue) => [
+      ...prevValue,
+      `data_${prevValue.length + 1}`,
+    ]);
+  };
+
+  const removeCards = (index) => {
+    const selectedCards = cardList[index];
+    const filteredData = cardList.filter((data) => data !== selectedCards);
+    //Update state
+    setCardList(filteredData);
+    console.log(cardList);
+  };
+
+  return (
+    <div>
+      {cardList.length === 0 && (
+        <Button
+          style={{ margin: "2rem" }}
+          variant="outlined"
+          color="error"
+          onClick={spawnCards}
+        >
+          START ADDING HERE
+        </Button>
+      )}
+      {cardList.map((item, index) => (
+        <Card style={{ margin: "3rem 2rem" }} key={index}>
+          <CardContent>{item}</CardContent>
+          {cardList.length - 1 === index && (
+            <Button variant="text" color="success" onClick={spawnCards}>
+              Add MORE
+            </Button>
+          )}
+          <Button
+            variant="text"
+            color="warning"
+            onClick={() => removeCards(index)}
+          >
+            REMOVE ITEM
+          </Button>
+        </Card>
+      ))}
+    </div>
+  );
 }
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,21 +1,12 @@
 import Navigation from "./Navigation";
 import Container from "@mui/material/Container";
-import { Routes, Route } from "react-router-dom";
-import CreateForm from "./CreateForm";
-import ResultForm from "./ResultForm";
-import AnswerForm from "./AnswerForm";
-import Landing from "./Landing";
+import Routes from "../routes/Routes";
 
 export default function Home() {
   return (
     <Container maxWidth="lg">
       <Navigation />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/Answer" element={<AnswerForm />} />
-        <Route path="/Create" element={<CreateForm />} />
-        <Route path="/Result" element={<ResultForm />} />
-      </Routes>
+      <Routes />
     </Container>
   );
 }
